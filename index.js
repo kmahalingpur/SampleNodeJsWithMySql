ea94d3b16957a66490cdb15f7d9eb0db4e8fe8c1
kmahalingpur <kmahalingpur@gmail.com>
2018-04-07 11:08:41 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,39 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+//var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var compression = require('compression');
+var api=require('./routes/api')
+
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+//app.use(logger('dev'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(compression());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(function (req, res, next) {
+    res.setHeader('X-Frame-Options', 'SAMEORIGIN');
+    res.setHeader('X-Content-Type-Options', 'nosniff');
+    res.setHeader('X-XSS-Protection', '1; mode=block');
+    next();
+});
+
+app.use('/api', api);
+
+
+
+module.exports = app;
